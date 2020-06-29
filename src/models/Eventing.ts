3ea5ed631e257395ff1b27ab84cf6bc87decd583
Maxondria <maxtayebwa@gmail.com>
2020-06-29 23:38:28 +0300

    wire up composition with delegation back into class User

--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -3,15 +3,15 @@ type Callback = () => void;
 export default class Eventing {
   public events: { [key: string]: Callback[] } = {};
 
-  on(eventName: string, callback: Callback): void {
+  on = (eventName: string, callback: Callback): void => {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
     this.events[eventName] = handlers;
-  }
+  };
 
-  trigger(eventName: string): void {
+  trigger = (eventName: string): void => {
     const handlers = this.events[eventName] || [];
     if (!handlers.length) return;
     handlers.forEach((callback: Callback) => callback());
-  }
+  };
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,7 @@
+import { AxiosResponse } from "axios";
 import Eventing from "./Eventing";
 import Sync from "./Sync";
+import Attributes from "./Attributes";
 
 export interface UserProps {
   id?: number;
@@ -12,6 +14,47 @@ const baseURL = "http://localhost:3004/users";
 export default class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(baseURL);
+  public attributes: Attributes<UserProps>;
 
+  public constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
 
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.get("id");
+
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without ID");
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    const data = this.attributes.getAll();
+    this.sync
+      .save(data)
+      .then((response: AxiosResponse): void => {
+        this.trigger("save");
+      })
+      .catch(() => this.trigger("error"));
+  }
 }
